f23a45d57825a90a07b85c2ca25ccd8a45322eae
ruthcfong <ruthcfong@gmail.com>
2012-01-16 19:13:57 -0500

    second commit - working- submits orders

--- pin.php
+++ pin.php
@@ -1,4 +1,5 @@
 <?php
+//echo 'hello!';
 
 function submitHTTPpost($cookie_file, $url, $isPOSTsubmission, $params)
 {
@@ -31,6 +32,8 @@ function submitHTTPpost($cookie_file, $url, $isPOSTsubmission, $params)
 // create cookie jar and direct to Harvard pin authentication site
 $cookie_file = tempnam(sys_get_temp_dir(), '');
 
+//$responseArr = array();
+
 // direct to Harvard pin authentication site using GET submission
 $response = submitHTTPpost($cookie_file, "https://www.pin1.harvard.edu/pin/authenticate?__authen_application=VPA_HUDS_MYHUDS", false, "");
 
@@ -47,30 +50,34 @@ if (count($matches) > 1)
 }
 
 // check whether the the scrip should submit an order
-if (empty($_POST["order"]))
+/*if (empty($_POST["order"]))
 {
   return;
-}
+}*/
 
 // submit order via GET submission
 $response = submitHTTPpost($cookie_file,$_POST['order'] ,false, "");
 //$response = submitHTTPpost($cookie_file,"http://www.dining.harvard.edu/myhuds/students/breakfast/?action=submit&pickup=2012-01-16&order%5Blocation_id%5D=FD&order%5Bdelivery_time%5D=07%3A00&pickup_list=0&items%5B8%20oz%20Cottage%20Cheese%5D=on",false, "");
 
 header("HTTP/1.1 200 OK");
-echo $response;
-$str = var_dump(file_get_contents($cookie_file));
+//echo $response;
+$str = (file_get_contents($cookie_file));
 
 // use a reg ex to check whether the login cookie has been activated
-preg_match('.www.dining.harvard.edu([\s]+)TRUE',$str, $matches);
+preg_match('/\.www\.dining\.harvard\.edu([\s]+)TRUE/',$str, $matches);
 if (count($matches) > 1)
 {
-  echo 'it worked!';
+  // echo 'it worked!';
+  $arr = array('didWork'=>'yes');
+  echo json_encode($arr);
   //header("HTTP/1.1 200 OK");
   //return $response;
 }
 else
 {
-  echo 'it failed =(';
+  $arr = array('didWork'=>'no');
+  //echo 'it failed =(';
+  echo json_encode($arr);
   //header("HTTP/1.1 200 OK");
   //return $response;
 }
